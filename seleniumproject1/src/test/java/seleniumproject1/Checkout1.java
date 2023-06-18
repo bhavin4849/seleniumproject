4ca2b32c92737d12df3855f597b46be415b65b8e
91953 <91953@DESKTOP-MO588CN>
2023-06-19 01:49:50 +0530

    Checkout1 updated

--- seleniumproject1/src/test/java/seleniumproject1/Checkout1.java
+++ seleniumproject1/src/test/java/seleniumproject1/Checkout1.java
@@ -6,12 +6,12 @@ import pageobject.SelectItem;
 public class Checkout1 extends TestCaseBase{
 	public void check()
 	{
-		driver.findElement(Checkout1.click_checkout).click();
-		driver.findElement(Checkout1.enter_firstname).sendKeys("Testf");
-		driver.findElement(Checkout1.enter_lastname).sendKeys("Testl");
-		driver.findElement(Checkout1.enter_postalcode).sendKeys("123456");
-		driver.findElement(Checkout1.coninue).click();
-		driver.findElement(Checkout1.finish).click();
+		driver.findElement(Checkout.click_checkout).click();
+		driver.findElement(Checkout.enter_firstname).sendKeys("Testf");
+		driver.findElement(Checkout.enter_lastname).sendKeys("Testl");
+		driver.findElement(Checkout.enter_postalcode).sendKeys("123456");
+		driver.findElement(Checkout.coninue).click();
+		driver.findElement(Checkout.finish).click();
 		
 	}
 }
