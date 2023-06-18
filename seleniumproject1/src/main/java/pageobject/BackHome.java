d04fa0e5751bf0c085c1237024199a4c82e4a85e
91953 <91953@DESKTOP-MO588CN>
2023-06-19 01:32:06 +0530

    backhome updated

--- seleniumproject1/src/main/java/pageobject/BackHome.java
+++ seleniumproject1/src/main/java/pageobject/BackHome.java
@@ -7,6 +7,4 @@ public class BackHome {
 	public static By click_burger_menu = By.xpath("//button[@id='react-burger-menu-btn']");
 	public static By logout = By.xpath("//a[@id='logout_sidebar_link']");
 }
-//driver.findElement(By.xpath("//button[@id='back-to-products']")).click();
-//driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']")).click();
-//driver.findElement(By.xpath("//a[@id='logout_sidebar_link']")).click();
+
