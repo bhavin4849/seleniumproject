6f2f68e5ecc734407515691a6057112bf44e976f
91953 <91953@DESKTOP-MO588CN>
2023-06-19 01:28:35 +0530

    Testcase 4 Updated

--- seleniumproject1/src/test/java/seleniumproject1/TestCase4.java
+++ seleniumproject1/src/test/java/seleniumproject1/TestCase4.java
@@ -1,11 +1,17 @@
 package seleniumproject1;
 
+import pageobject.LoginPage;
 import pageobject.ShowCart;
+import seleniumproject1.Login;
 
 public class TestCase4 extends TestCaseBase{
+	Login l = new Login();
 	public void showCart()
 	{
-		setup();
+		//l.login();
+		/*driver.findElement(LoginPage.txt_username).sendKeys("standard_user");
+		driver.findElement(LoginPage.txt_password).sendKeys("secret_sauce");
+		driver.findElement(LoginPage.btn_login).click();*/
 		driver.findElement(ShowCart.show_cart).click();
 	}
 }
